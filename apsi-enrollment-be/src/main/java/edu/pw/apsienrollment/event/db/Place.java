686c57206f699b0f3173d81f8b2266d06c25171c
Delebrith <paulina.szwed.1996@gmail.com>
2020-04-05 21:32:28 +0200

    add data model for events and meetings (no enrollments yet). add sample data

---
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/db/Place.java
@@ -0,0 +1,31 @@
+package edu.pw.apsienrollment.event.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "PLACE")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String building;
+
+    @Column(nullable = false, length = 500)
+    private String address;
+
+    @Column(nullable = false)
+    private Integer capacity;
+}
