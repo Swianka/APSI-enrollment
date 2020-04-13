9515b47fd15cb16d80944c1c32296f2a5ce0c465
gala377 <r.galczak@gmail.com>
2020-04-13 14:15:47 +0200

    Added simple SearCriteria and search paramter to the events endpoint

--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/EventServiceImpl.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/EventServiceImpl.java
@@ -2,10 +2,12 @@ package edu.pw.apsienrollment.event;
 
 import edu.pw.apsienrollment.event.db.Event;
 import edu.pw.apsienrollment.event.db.EventRepository;
+import edu.pw.apsienrollment.event.specification.EventSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -22,8 +24,25 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public Page<Event> findAll(Integer page, Integer pageSize) {
+        checkPagingParameters(page, pageSize);
+        return eventRepository.findAll(PageRequest.of(page, pageSize));
+    }
+
+    @Override
+    public Collection<Event> findAll(Specification<Event> spec) {
+        return eventRepository.findAll(spec);
+    }
+
+    @Override
+    public Page<Event> findAll(Specification<Event> spec, Integer page, Integer pageSize) {
+        checkPagingParameters(page, pageSize);
+        return eventRepository.findAll(spec, PageRequest.of(page, pageSize));
+    }
+
+    static void checkPagingParameters(Integer page, Integer pageSize) {
+        if (page == null) throw new RuntimeException("ValueError: Page cannot be null");
+        if (pageSize == null) throw new RuntimeException("ValueError: Page size cannot be null");
         if (page < 0) throw new RuntimeException("ValueError: Page cannot be negative");
         if (pageSize <= 0) throw new RuntimeException(("ValueError: Page size should be greater than 0"));
-        return eventRepository.findAll(PageRequest.of(page, pageSize));
     }
 }
--- apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/api/EventController.java
+++ apsi-enrollment-be/src/main/java/edu/pw/apsienrollment/event/api/EventController.java
@@ -1,14 +1,22 @@
 package edu.pw.apsienrollment.event.api;
 
-import edu.pw.apsienrollment.event.EventService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import lombok.NonNull;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import lombok.RequiredArgsConstructor;
+
+import edu.pw.apsienrollment.event.EventService;
+import edu.pw.apsienrollment.event.db.Event;
+import edu.pw.apsienrollment.event.specification.EventSpecificationBuilder;
 
 @RestController
 @RequestMapping("events")
@@ -16,12 +24,74 @@ import java.util.stream.Collectors;
 public class EventController {
     private final EventService eventService;
 
-    @GetMapping("all")
-    ResponseEntity<List<EventDto>> findAll() {
-        return ResponseEntity.ok(eventService
-                .findAll()
+    @GetMapping("")
+    ResponseEntity<Iterable<EventDto>> events(
+            @RequestParam Optional<Integer> page,
+            @RequestParam Optional<Integer> size,
+            @RequestParam Optional<String> search) {
+        if(search.isPresent()) {
+            if(checkPageParams(page, size)) {
+                return this.search(search.get(), page.get(), size.get());
+            }
+            return this.search(search.get());
+        }
+        if(checkPageParams(page, size)) {
+            return findAll(page.get(), size.get());
+        }
+        return findAll();
+    }
+
+    static Boolean checkPageParams(Optional<Integer> page, Optional<Integer> size) {
+        if(page.isPresent() || size.isPresent()) {
+            if(page.isPresent() && size.isPresent()) {
+                if(page.get() < 0) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You cannot fetch negative page");
+                }
+                if(size.get() < 1) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Page size cannot be less than 1");
+                }
+                return true;
+            }
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Page and Size have to be both specified");
+        }
+        return false;
+    }
+
+    ResponseEntity<Iterable<EventDto>> findAll() {
+        return ResponseEntity.ok(eventService.findAll()
                 .stream()
                 .map(EventDto::of)
                 .collect(Collectors.toList()));
+    }
+
+    ResponseEntity<Iterable<EventDto>> findAll(Integer page, Integer pageSize) {
+        return ResponseEntity.ok(eventService.findAll(page, pageSize)
+                .map(EventDto::of));
+    }
+
+    ResponseEntity<Iterable<EventDto>> search(String query) {
+        Specification<Event> spec = eventSpecFrom(query);
+        return ResponseEntity.ok(eventService.findAll(spec)
+                .stream()
+                .map(EventDto::of)
+                .collect(Collectors.toList()));
+    }
+
+    ResponseEntity<Iterable<EventDto>> search(String query, Integer page, Integer size) {
+        Specification<Event> spec = eventSpecFrom(query);
+        System.out.println("Searching");
+        System.out.println("Query is " + query);
+        return ResponseEntity.ok(eventService.findAll(spec, page, size)
+                .map(EventDto::of));
+    }
+
+    static Specification<Event> eventSpecFrom(@NonNull String query) {
+        EventSpecificationBuilder builder = new EventSpecificationBuilder();
+        Pattern pattern = Pattern.compile("([\\w]+?)([:<>])(\\w+?),", Pattern.UNICODE_CHARACTER_CLASS);
+        Matcher matcher = pattern.matcher(query + ",");
+        while(matcher.find()) {
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+        return builder.build();
     }
 }
