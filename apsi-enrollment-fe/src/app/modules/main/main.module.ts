123d54c02ab8e37d814b126da91bcb2d723f7dcc
Wojciech Gruszka <wojciech@gruszka.eu>
2020-04-13 17:45:44 +0200

    Added pages and routing

--- apsi-enrollment-fe/src/app/modules/main/main.module.ts
+++ apsi-enrollment-fe/src/app/modules/main/main.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CoreModule } from 'src/app/core/core.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main.routing';
+import { AllEventsComponent } from './pages/all-events/all-events.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MainComponent, AllEventsComponent],
   imports: [CommonModule, MainRoutingModule, CoreModule, SharedModule],
 })
 export class MainModule {}
--- apsi-enrollment-fe/src/app/modules/main/main.routing.ts
+++ apsi-enrollment-fe/src/app/modules/main/main.routing.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main.component';
+import { AllEventsComponent } from './pages/all-events/all-events.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent,
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'all-events',
+        component: AllEventsComponent,
+      },
+      {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+    ],
   },
   {
     path: '**',
     pathMatch: 'full',
-    redirectTo: 'home',
+    redirectTo: '',
   },
 ];
 
